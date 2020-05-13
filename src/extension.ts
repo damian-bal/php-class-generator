b2f43af960dae97ab8ff50b3426d4de4d1bc0e52
damian-bal <balandowskie+git@gmail.com>
2020-05-13 15:03:47 +0100

    added commands to inserting namespace and class/trait/interface

--- src/extension.ts
+++ src/extension.ts
@@ -81,6 +81,117 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		)
 	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'phpClassGenerator.insertNamespace',
+			async () => {
+				if (!vscode.window.activeTextEditor) {
+					return;
+				}
+
+				if (!vscode.workspace && !vscode.workspace.name) {
+					vscode.window.showWarningMessage("You need to have a workspace open!");
+					return;
+				}
+
+				const root = vscode.workspace.rootPath;
+
+				let namespacePathSegments = vscode.window.activeTextEditor.document.fileName.split(path.sep);
+				namespacePathSegments.pop();
+				namespacePathSegments = vscode.workspace.asRelativePath(namespacePathSegments.join(path.sep)).split(path.sep);
+
+				let composerDocument = null;
+				try {
+					composerDocument = await vscode.workspace.openTextDocument(root + path.sep + 'composer.json');
+				} catch (error) {
+					console.error(error);
+					vscode.window.showErrorMessage("Problem reading composer.json file!");
+					return;
+				}
+
+				const namespaceResolver: NamespaceResolver = new ComposerNamespaceResolver(composerDocument.getText());
+				const namespace = namespaceResolver.resolve(namespacePathSegments);
+
+				if (!namespace) {
+					vscode.window.showErrorMessage("Problem with resolving namespace!");
+					return;
+				}
+
+				const editor = vscode.window.activeTextEditor;
+				editor.edit(editBuilder => editBuilder.replace(
+					new vscode.Position(editor.selection.active.line, 0),
+					`namespace ${namespace};`
+				));
+			}
+		)
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'phpClassGenerator.insertClass',
+			async () => {
+				if (!vscode.window.activeTextEditor) {
+					return;
+				}
+
+				if (!vscode.workspace && !vscode.workspace.name) {
+					vscode.window.showWarningMessage("You need to have a workspace open!");
+					return;
+				}
+
+				const root = vscode.workspace.rootPath;
+
+				let namespacePathSegments = vscode.window.activeTextEditor.document.fileName.split(path.sep);
+				const name = namespacePathSegments.pop()?.replace('.php', '');
+				namespacePathSegments = vscode.workspace.asRelativePath(namespacePathSegments.join(path.sep)).split(path.sep);
+
+				let composerDocument = null;
+				try {
+					composerDocument = await vscode.workspace.openTextDocument(root + path.sep + 'composer.json');
+				} catch (error) {
+					console.error(error);
+					vscode.window.showErrorMessage("Problem reading composer.json file!");
+					return;
+				}
+
+				const namespaceResolver: NamespaceResolver = new ComposerNamespaceResolver(composerDocument.getText());
+				const namespace = namespaceResolver.resolve(namespacePathSegments);
+
+				if (!namespace) {
+					vscode.window.showErrorMessage("Problem with resolving namespace!");
+					return;
+				}
+
+				const type = await vscode.window.showQuickPick(
+					[
+						"class",
+						"interface",
+						"trait"
+					],
+					{
+						placeHolder: "Select what to generate"
+					}
+				);
+
+				if (!type) {
+					return;
+				}
+
+				const editor = vscode.window.activeTextEditor;
+				editor.edit(editBuilder => editBuilder.replace(
+					new vscode.Position(editor.selection.active.line, 0),
+					generatePhpFileContent(
+						{
+							name: name ?? 'Uknown',
+							type,
+							namespace
+						}
+					)
+				));
+			}
+		)
+	);
 }
 
 export function deactivate() { }
