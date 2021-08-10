dd1eaa77ae624303d9f7577a7f37fcd2e079e36a
Juan González Montes <juangonzalezmontes@gmail.com>
2021-08-10 22:48:30 +0200

    feature/add-declare-strict-types
    - Add declare strict types statement before namespace

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,8 @@
 export function generatePhpFileContent(data: { type: string, name: string, namespace: string }): string {
     let content = "<?php\n";
     content += "\n";
+    content += "declare(strict_types=1);"
+    content += "\n";
     content += `namespace ${data.namespace};\n`;
     content += "\n";
     content += `${data.type} ${data.name}\n`;
